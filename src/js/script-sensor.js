89824a9138db806649add1f31a596509232d61fb
Joc4A <136185062+Joc4A@users.noreply.github.com>
2025-10-26 20:56:47 -0300

    Update script-sensor.js

--- src/js/script-sensor.js
+++ src/js/script-sensor.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
-  // const client = mqtt.connect('wss://broker.emqx.io:8084/mqtt');
-  const client = mqtt.connect('wss://broker.emqx.io:8084/mqtt');
+    const client = mqtt.connect('ws://54.233.175.183:8083/mqtt', {
+    clientId: 'emqx_NTEyNz',
+  });
 
   client.on('connect', () => {
     setTimeout(infoHide, 1000);
